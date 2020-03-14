1a665040beac9f62178be5b7a73949ac7f72abf6
Bobby Powers <bobbypowers@gmail.com>
2020-03-14 15:08:00 -0700

    heaplayers-common.h: add a bunch of defines from mesh

--- heaplayers-common.h
+++ heaplayers-common.h
@@ -7,14 +7,11 @@
 #ifndef HL_HEAPLAYERS_COMMON_H
 #define HL_HEAPLAYERS_COMMON_H
 
-#if defined(__linux__) || defined(__APPLE__)
-#define HL_EXPORT __attribute__((visibility("default")))
-#else
-#define HL_EXPORT
-#endif
-
-#define HL_ATTRIBUTE_NEVER_INLINE __attribute__((noinline))
-#define HL_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+namespace HL {
+// Good for practically all platforms.
+static constexpr size_t kPageSize = 4096;
+static constexpr size_t kCachelineSize = 64;
+}  // namespace HL
 
 // Define HL_EXECUTABLE_HEAP as 1 if you want that (i.e., you're doing
 // dynamic code generation).
@@ -32,13 +29,37 @@
 #define HL_MALLOC_FUNCTION
 #define HL_RESTRICT
 
-// GNU C
-#elif defined(__GNUC__)
+#define HL_ATTRIBUTE_ALWAYS_INLINE
+#define HL_ATTRIBUTE_NEVER_INLINE
+#define HL_ATTRIBUTE_INITIAL_EXEC
+#define HL_ATTRIBUTE_NORETURN
+#define HL_ATTRIBUTE_ALIGNED(s)
+#define HL_CACHELINE_ALIGNED
+#define HL_PAGE_ALIGNED
+#define HL_EXPORT
+
+#define HL_LIKELY(x) x
+#define HL_UNLIKELY(x) x
+
+// gcc and clang
+#elif defined(__GNUC__) || defined(__clang__)
 
 #define HL_INLINE inline
 #define HL_NOINLINE __attribute__((noinline))
 #define HL_MALLOC_FUNCTION __attribute__((malloc))
 #define HL_RESTRICT __restrict__
+
+#define HL_ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+#define HL_ATTRIBUTE_NEVER_INLINE __attribute__((noinline))
+#define HL_ATTRIBUTE_INITIAL_EXEC __attribute__((tls_model("initial-exec")))
+#define HL_ATTRIBUTE_NORETURN __attribute__((noreturn))
+#define HL_ATTRIBUTE_ALIGNED(s) __attribute__((aligned(s)))
+#define HL_CACHELINE_ALIGNED HL_ATTRIBUTE_ALIGNED(HL::kCachelineSize)
+#define HL_PAGE_ALIGNED HL_ATTRIBUTE_ALIGNED(HL::kPageSize)
+#define HL_EXPORT __attribute__((visibility("default")))
+
+#define HL_LIKELY(x) __builtin_expect(!!(x), 1)
+#define HL_UNLIKELY(x) __builtin_expect(!!(x), 0)
 
 // All others
 #else
@@ -48,9 +69,28 @@
 #define HL_MALLOC_FUNCTION
 #define HL_RESTRICT
 
-#endif // defined(_MSC_VER)
+#define HL_ATTRIBUTE_ALWAYS_INLINE
+#define HL_ATTRIBUTE_NEVER_INLINE
+#define HL_ATTRIBUTE_INITIAL_EXEC
+#define HL_ATTRIBUTE_NORETURN
+#define HL_ATTRIBUTE_ALIGNED(s)
+#define HL_CACHELINE_ALIGNED
+#define HL_PAGE_ALIGNED
+#define HL_EXPORT
 
-#define HL_LIKELY(x) __builtin_expect(!!(x), 1)
-#define HL_UNLIKELY(x) __builtin_expect(!!(x), 0)
+#define HL_LIKELY(x) x
+#define HL_UNLIKELY(x) x
+
+#endif  // defined(_MSC_VER)
+
+// HL_HAVE_TLS is defined to 1 when __thread should be supported.  We
+// assume __thread is supported on Linux when compiled with Clang or
+// compiled against libstdc++ with _GLIBCXX_HAVE_TLS defined. (this
+// check is from Abseil)
+#ifdef HL_HAVE_TLS
+#error HL_HAVE_TLS cannot be directly set
+#elif defined(__linux__) && (defined(__clang__) || defined(_GLIBCXX_HAVE_TLS))
+#define HL_HAVE_TLS 1
+#endif
 
 #endif  // HL_HEAPLAYERS_COMMON_H
